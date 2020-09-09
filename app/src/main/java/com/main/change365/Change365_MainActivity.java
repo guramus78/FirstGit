f19466548e65be67dca47256f866a4af09a0473f
guramus78 <ratoumin@naver.com>
2020-09-09 23:36:34 +0900

    First Git Test

---
+++ app/src/main/java/com/main/change365/Change365_MainActivity.java
@@ -0,0 +1,14 @@
+package com.main.change365;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Change365_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change365__main);
+    }
+}
